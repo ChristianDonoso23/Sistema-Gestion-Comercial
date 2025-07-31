95a8321cd40163cb7eb4bc9a375624d6b865fa30
Christian Donoso <cedonoso@espe.edu.ec>
2025-07-30 22:56:59 -0500

    Característica: Configuración de parámetros de conexión a la base de datos en la clase Database

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -13,12 +13,12 @@ class Database
     {
         if (self::$instance === null)
         {
-            $host = '';
-            $dbname = '';
-            $username = '';
-            $password = '';
-            $charset = 'utf8mb4';
-            $dns = "mysql:host={$host};dname={$dbname};charset={$charset}";
+            $host       = 'localhost';
+            $dbname     = 'sistema_gestion_comercial';
+            $username   = 'root';
+            $password   = '';
+            $charset    = 'utf8mb4';
+            $dns = "mysql:host={$host};dbname={$dbname};charset={$charset}";
             $option = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
